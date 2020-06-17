ac7773052232c80fdb6086e6d0b241653d3cf643
magb7 <margaux.bedu@gmail.com>
2020-06-17 17:34:20 +0200

    wrote function

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,7 +1,17 @@
 const assert = require("assert");
+const { stringify } = require("querystring");
 
 // FUNCTION
-const capitalizeFirstLetters = (input) => {};
+const capitalizeFirstLetters = (input) => {
+  if (input.length > 0) {
+    let splitInput = input.split(" ");
+    for (let i = 0; i < splitInput.length; i++) {
+      splitInput[i] =
+        splitInput[i].charAt(0).toUpperCase() + splitInput[i].substring(1);
+    }
+    return splitInput.join(" ");
+  } else return "";
+};
 
 // TESTS
 assert.strictEqual(typeof capitalizeFirstLetters, "function");
